a277a792e452d95c4ee6ea87a7f013d64d690b1e
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-09-16 16:33:41 +0530

    Fix formatting in vite.config.js for base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/currency_converter/"
+  base:"/currency_converter/",
   plugins: [react()],
 })
